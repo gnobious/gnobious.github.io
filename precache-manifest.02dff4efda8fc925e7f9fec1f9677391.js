9d99dafc1e67220079be7f72ee8cafd85ad112d7
Roman <rimer93@gmail.com>
2021-03-05 11:55:12 +0300

    Updates

---
+++ precache-manifest.02dff4efda8fc925e7f9fec1f9677391.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c2a0d40c2f97960d31943498b1ad5c",
+    "url": "/Projects/Resto/index.html"
+  },
+  {
+    "revision": "6cbac6653cea17aa96f0",
+    "url": "/Projects/Resto/static/css/main.2ed0627f.chunk.css"
+  },
+  {
+    "revision": "807cf0ccc942a43bd8de",
+    "url": "/Projects/Resto/static/js/2.48060bdf.chunk.js"
+  },
+  {
+    "revision": "6cbac6653cea17aa96f0",
+    "url": "/Projects/Resto/static/js/main.b628dd62.chunk.js"
+  },
+  {
+    "revision": "996569302b17ea11288e",
+    "url": "/Projects/Resto/static/js/runtime~main.49b28858.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/Projects/Resto/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/Projects/Resto/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
